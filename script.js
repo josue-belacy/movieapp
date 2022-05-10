5648ee72b68c0e65baeaac92fdad8bf0c619cb97
Josue Belacy <jbelacy@gmail.com>
2022-05-10 01:29:20 -0400

    Created async function

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
 const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=fd83167750aea52bd28bde79618d45f&query="'
 
+const form = document.getElementById('form')
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+}
