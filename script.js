ae7e5322ad0cb9a8024dde7cb2883c170ab85467
Josue Belacy <jbelacy@gmail.com>
2022-05-09 20:35:47 -0400

    Began Implementing Movie Api

---
+++ script.js
@@ -0,0 +1,3 @@
+const API_URL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2fd83167750aea52bd28bde79618d45f&page=1"
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w500"
