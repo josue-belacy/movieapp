14f698caf31b3fc298390f2fbb08c25a33940a1e
Josue Belacy <jbelacy@gmail.com>
2022-05-10 01:34:54 -0400

    Search API created

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2fd83167750aea52bd28bde79618d45f&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=fd83167750aea52bd28bde79618d45f&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fd83167750aea52bd28bde79618d45f&query="'
 
 const form = document.getElementById('form')
-
+const search = document.getElementById('search')
 // Get initial movies
 getMovies(API_URL)
 
@@ -13,3 +13,17 @@ async function getMovies(url) {
 
   console.log(data.results)
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search
+
+  if(searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
