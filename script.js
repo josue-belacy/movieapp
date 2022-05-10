ffd7e977336816eebe86ff06b1629dc556cc39f2
Josue Belacy <jbelacy@gmail.com>
2022-05-09 20:46:47 -0400

    Created an API Key

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-const API_URL ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2fd83167750aea52bd28bde79618d45f&page=1"
+const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2fd83167750aea52bd28bde79618d45f&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=fd83167750aea52bd28bde79618d45f&query="'
 
-const IMG_PATH = "https://image.tmdb.org/t/p/w500"
