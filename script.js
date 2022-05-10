9a3bc167a3744f6104c734a8e9dfb3a25a07130c
Josue Belacy <jbelacy@gmail.com>
2022-05-10 01:55:07 -0400

    Created loop for movie data

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2fd83167750aea52bd28bde79618d45f&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fd83167750aea52bd28bde79618d45f&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=2fd83167750aea52bd28bde79618d45f&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+
 // Get initial movies
 getMovies(API_URL)
 
@@ -11,7 +13,15 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHtml = ''
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+  })
 }
 
 form.addEventListener('submit', (e) => {
